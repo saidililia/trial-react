27184352f189acd718d833c45de66d95082953fb
saidililia <saidililia555@gmail.com>
2024-05-23 10:11:04 +0100

    netlify react

--- src/components/payment/payment.js
+++ src/components/payment/payment.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import { Button, Card, Col, Row } from 'antd';
